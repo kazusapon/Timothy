26fb3f5fca0a8e97308ea0fdc58bcfd58ffd46ff
kazusapon <flum1278pool@gmail.com>
2020-09-20 07:19:24 +0900

    初期コミット

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Timothy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
