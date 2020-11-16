75f84b18c6585811602ddb31e1635467651df5a7
kazusapon <flum1278pool@gmail.com>
2020-11-17 00:21:13 +0900

    これもリレーション見直し

--- Models/Entities/GuestType.cs
+++ Models/Entities/GuestType.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -9,5 +10,9 @@ namespace EntityModels
         public int Id {get; set;}
 
         public string GuestTypeName {get; set;}
+
+        public virtual ICollection<EntityModels.Inquiry> Inquiries { get; set; }
+
+        public virtual ICollection<EntityModels.CallRegister> CallRegisters { get; set; }
     }
 }
--- Models/Summary/ISummary.cs
+++ Models/Summary/ISummary.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SummaryModel
+namespace Summary.Model
 {
     public interface ISummary
     {
-
+        Task<ChartModel> BuildEachSystemInquiryCountAndMonthly();
     }
 }
